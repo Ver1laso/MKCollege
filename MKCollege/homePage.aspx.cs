b756e22fefe2c822fdb33246877154df8fb26240
JoseS <335154@edu.mkcollege.ac.uk>
2023-04-25 21:08:08 +0100

    User profile v1

--- MKCollege/homePage.aspx.cs
+++ MKCollege/homePage.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace MKCollege
 {
-    public partial class Home_Page : System.Web.UI.Page
+    public partial class homePage : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
