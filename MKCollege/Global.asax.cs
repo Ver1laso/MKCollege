1e24d822b440995ef66797bfb7064486cde9535b
Joshua James Verner <joshuaverner123@gmail.com>
2023-06-05 15:26:23 +0100

    fix final hopefully

--- MKCollege/Global.asax.cs
+++ MKCollege/Global.asax.cs
@@ -12,29 +12,29 @@ namespace MKCollege
     public class Global : System.Web.HttpApplication
     {
 
-        protected void Application_Start(object sender, EventArgs e) 
+        protected void Application_Start(object sender, EventArgs e)
         {
             RouteTable.Routes.MapOwinPath("/signalr", builder =>
             {
                 builder.RunSignalR();
             });
         }
-        protected void Session_Start(object sender, EventArgs e) 
-        { 
-        }
-        protected void Application_BeginRequest(object sender, EventArgs e) 
+        protected void Session_Start(object sender, EventArgs e)
         {
         }
-        protected void Application_AuthenticateRequest(object sender, EventArgs e) 
+        protected void Application_BeginRequest(object sender, EventArgs e)
         {
         }
-        protected void Application_Error(object sender, EventArgs e) 
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
         {
         }
-        protected void Session_End(object sender, EventArgs e) 
+        protected void Application_Error(object sender, EventArgs e)
         {
         }
-        protected void Application_End(object sender, EventArgs e) 
+        protected void Session_End(object sender, EventArgs e)
+        {
+        }
+        protected void Application_End(object sender, EventArgs e)
         {
         }
 
