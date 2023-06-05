2ab5c712d3674b95a97685cb39279db569239767
JoseS <335154@edu.mkcollege.ac.uk>
2023-06-05 11:20:27 +0100

    Monday 5th June, messages working

--- MKCollege/messages.aspx.designer.cs
+++ MKCollege/messages.aspx.designer.cs
@@ -15,39 +15,12 @@ namespace MKCollege
     {
 
         /// <summary>
-        /// messageRepeater control.
+        /// form1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Repeater messageRepeater;
-
-        /// <summary>
-        /// txtMessage control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox txtMessage;
-
-        /// <summary>
-        /// btnSend control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnSend;
-
-        /// <summary>
-        /// lnkLoadMore control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.HyperLink lnkLoadMore;
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
     }
 }
