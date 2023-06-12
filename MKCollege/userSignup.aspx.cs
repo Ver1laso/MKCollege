54643399b9d38781d2081d1ec22af4a3db265d36
SittingDuck <335153@edu.mkcollege.ac.uk>
2023-06-12 15:24:40 +0100

    Usersignup completed. userid cookie created, populates fields in profile following signup.

--- MKCollege/userSignup.aspx.cs
+++ MKCollege/userSignup.aspx.cs
@@ -3,48 +3,50 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Xml.Linq;
 
 namespace MKCollege
 {
-
     public partial class userSignup : System.Web.UI.Page
     {
         string strcon = ConfigurationManager.ConnectionStrings["MKConnectDatabase"].ConnectionString;
+
         protected void Page_Load(object sender, EventArgs e)
         {
-        }
-        // sign up button click event
-        protected void Button1_Click(object sender, EventArgs e)
-        {
-            {
-                signUpNewMember();
-            }
+            Button1.Click += new EventHandler(this.SignUpBtn_Click);
         }
 
-        // user defined method
-        void signUpNewMember()
+        // Sign up button click event
+        void SignUpBtn_Click(object sender, EventArgs e)
         {
-            //Response.Write("<script>alert('Testing');</script>");
             try
             {
-                SqlConnection con = new SqlConnection(strcon);
-                if (con.State == ConnectionState.Closed)
+                using (SqlConnection con = new SqlConnection(strcon))
                 {
                     con.Open();
+                    SqlCommand cmd = new SqlCommand("INSERT INTO Users(FirstName, LastName, Email, DOB, IsMentor) VALUES (@FirstName, @LastName, @Email, @DOB, @IsMentor); SELECT SCOPE_IDENTITY();", con);
+                    cmd.Parameters.AddWithValue("@FirstName", TextBox1.Text.Trim());
+                    cmd.Parameters.AddWithValue("@LastName", TextBox4.Text.Trim());
+                    cmd.Parameters.AddWithValue("@Email", TextBox5.Text.Trim());
+                    cmd.Parameters.AddWithValue("@DOB", TextBox3.Text.Trim());
+                    cmd.Parameters.AddWithValue("@IsMentor", false);
+
+                    int insertedUserId = Convert.ToInt32(cmd.ExecuteScalar());
+
+                    HttpCookie IdCookie = new HttpCookie("Id");
+                    IdCookie.Value = insertedUserId.ToString();
+                    Response.Cookies.Add(IdCookie);
+
+                    Response.Write("<script>alert('Sign Up Successful.');</script>");
+                    Response.Redirect("userProfile.aspx");
                 }
-                SqlCommand cmd = new SqlCommand("INSERT INTO Users(FirstName, LastName, Email, DOB, IsMentor) Values(@FirstName, @LastName, @Email, @DOB, @IsMentor)", con);
-                cmd.Parameters.AddWithValue("@FirstName", TextBox1.Text.Trim());
-                cmd.Parameters.AddWithValue("@LastName", TextBox4.Text.Trim());
-                cmd.Parameters.AddWithValue("@Email", TextBox5.Text.Trim());
-                cmd.Parameters.AddWithValue("@DOB", TextBox3.Text);
-                cmd.Parameters.AddWithValue("@IsMentor", false);
-                cmd.ExecuteNonQuery();
-                con.Close();
-                Response.Write("<script>alert('Sign Up Successful. Go to User Login to Login');</script>");
             }
             catch (Exception ex)
             {
